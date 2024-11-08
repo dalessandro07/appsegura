1692f78cb03000ae5b1a0afb73297feb03b71a54
Alessandro Rios <drios28@outlook.es>
2024-11-08 12:56:03 -0500

    feat: enhance login and registration pages with user session handling and navigation

--- src/app/(routes)/login/page.tsx
+++ src/app/(routes)/login/page.tsx
@@ -1,9 +1,45 @@
 import LoginForm from '@/app/(routes)/login/components/LoginForm'
+import { getSession } from '@/app/actions/action-session'
+import { EarthLockIcon } from 'lucide-react'
 import Link from 'next/link'
 
-export default function LoginPage () {
+export default async function LoginPage () {
+  const { user } = await getSession()
+
   return (
     <main className='p-5 flex flex-col gap-10'>
+      <header className='flex justify-between items-center'>
+        <Link href='/' className='flex items-center gap-1'>
+          <EarthLockIcon size={32} />
+          <span className='text-xs font-bold'>AppSegura</span>
+        </Link>
+
+        {user ? (
+          <div className='flex items-center gap-5'>
+            Bienvenido, {user.nombre_comisaria} - ({user.dni})
+          </div>
+        ) : (
+          <nav>
+            <ul className='flex gap-2 text-sm'>
+              <li>
+                <Link className='underline' href='/login'>
+                  Iniciar sesión
+                </Link>
+              </li>
+              <li>
+                <Link className='underline' href='/register'>
+                  Registrarse
+                </Link>
+              </li>
+              <li>
+                <Link className='underline' href='/reportes'>
+                  Reportes
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        )}
+      </header>
       <h1 className='font-bold text-2xl'>Inicia sesión</h1>
 
       <LoginForm />
--- src/app/(routes)/register/page.tsx
+++ src/app/(routes)/register/page.tsx
@@ -1,9 +1,46 @@
 import RegisterForm from '@/app/(routes)/register/components/RegisterForm'
+import { getSession } from '@/app/actions/action-session'
+import { EarthLockIcon } from 'lucide-react'
 import Link from 'next/link'
 
-export default function RegisterPage () {
+export default async function RegisterPage () {
+  const { user } = await getSession()
+
   return (
     <main className='p-5 flex flex-col gap-10'>
+      <header className='flex justify-between items-center'>
+        <Link href='/' className='flex items-center gap-1'>
+          <EarthLockIcon size={32} />
+          <span className='text-xs font-bold'>AppSegura</span>
+        </Link>
+
+        {user ? (
+          <div className='flex items-center gap-5'>
+            Bienvenido, {user.nombre_comisaria} - ({user.dni})
+          </div>
+        ) : (
+          <nav>
+            <ul className='flex gap-2 text-sm'>
+              <li>
+                <Link className='underline' href='/login'>
+                  Iniciar sesión
+                </Link>
+              </li>
+              <li>
+                <Link className='underline' href='/register'>
+                  Registrarse
+                </Link>
+              </li>
+              <li>
+                <Link className='underline' href='/reportes'>
+                  Reportes
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        )}
+      </header>
+
       <h1 className='text-2xl font-bold'>Crea tu cuenta</h1>
 
       <RegisterForm />
